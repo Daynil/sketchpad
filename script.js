ad957838b39796d68cc4352ebb5d0a699d4048ad
Danny Libin <omegasol11@gmail.com>
2014-12-01 14:16:36 -0600

    completed base sketchpad app, may add more functionality later

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 var $containerDiv = $('<div></div>');
+var newGridSize;
+var viewportHeight;
+var viewportWidth;
 
 $(document).ready(function() {
-	$('body').append($containerDiv);
+	viewportHeight = $(window).height();
+	viewportWidth = $(document).width();
+	$('html').css('margin', '0');
+	$('html').css('padding', '0');
 	addButton();
-	createGrid(16, 16);
+	$('body').append($containerDiv);
+	resetBoard();
 });
 
-var createGrid = function(rows, columns) {
+var createGrid = function(rows, columns, gridUnitSize) {
+
 	for (var row=0; row<rows; row++) {
 		var $rowContainer = $('<div style="float: left; clear: both;"></div>');
 		$containerDiv.append($rowContainer);
 		for (var col=0; col<columns; col++) {
-			var $GridUnit = $('<div class="grid_unit" style="width: 50px; height:50px; background-color: green; float: left;"></div>');
+			var $GridUnit = $('<div class="grid_unit" style="background-color: green; float: left;"></div>');
+			$GridUnit.css('width', gridUnitSize.toString());
+			$GridUnit.css('height', gridUnitSize.toString());
 			$rowContainer.append($GridUnit);
 		}
 	}
@@ -21,12 +31,38 @@ var createGrid = function(rows, columns) {
 }
 
 var addButton = function() {
-	$containerDiv.append('<button id="clear_button" style="float: left; margin-bottom: 10px;">Clear</button>');
+	$('body').append('<div id="button_container" style="float: left; clear:both;"></div>')
+	$('#button_container').append('<button id="clear_button" style="float: left; margin-bottom: 10px;">Clear</button>');
 	$('#clear_button').on('click', function() {
 		resetBoard();
 	});
 }
 
 var resetBoard = function() {
-	$('body').remove($containerDiv);
+	$containerDiv.empty();
+	viewportHeight = $(window).height();
+	viewportWidth = $(window).width();
+	newGridSize = Number(window.prompt("How many pixels per row do you want? (Enter one number)", newGridSize));
+	if (newGridSize === null) {
+		newGridSize = 20;
+	}
+	while (isNaN(newGridSize) || newGridSize < 6) {
+		newGridSize = Number(window.prompt("Grid size must be a positive integer over 5", newGridSize));
+	}
+	var gridRows;
+	if (newGridSize <= 10) {
+		gridRows = newGridSize - Math.floor(newGridSize*.2);
+	} else {
+		gridRows = newGridSize - Math.floor(newGridSize*.05);
+	}
+
+	var gridCols;
+	var gridUnitSize = viewportHeight/newGridSize;
+	if (newGridSize <= 30) {
+		gridCols = (viewportWidth/gridUnitSize) - Math.floor((viewportWidth/gridUnitSize)*.1);
+	} else {
+		gridCols = (viewportWidth/gridUnitSize) - Math.floor((viewportWidth/gridUnitSize)*.05);
+	}
+	
+	createGrid(gridRows, gridCols, gridUnitSize);
 }
